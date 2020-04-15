b3447b8478d4e0342bad9256c482e96621d6906c
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-04-15 02:05:28 -0700

    Add files via upload

---
+++ square.js
@@ -0,0 +1,8 @@
+var isSquare = function(n){
+    half = n/2;                 //get half the number
+    if (n == half * 2 && n >= 0){        // conditional statement that totally misses the mark
+      return true
+  }
+    else
+    return false;
+  }
