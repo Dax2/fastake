90e73cf82e6be5f0c216fcce77c29a119a581039
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-04-17 04:12:26 -0700

    Add files via upload

---
+++ methods_square.js
@@ -0,0 +1,10 @@
+var isSquare = function(n){
+    squareRoot = Math.sqrt(n);
+     if ( n >= 0){
+       intCheck = Number.isInteger(squareRoot);
+        if (intCheck = true){
+        return true
+       }
+   }    
+       else return false;
+   } 
