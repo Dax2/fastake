6451251bd55dbbdccb42aeb0e776448059daaf4e
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-04-22 01:14:26 -0700

    Add files via upload

--- aSolution.js
+++ aSolution.js
@@ -6,3 +6,19 @@ var isSquare = function(n){
       return false;
   }
   
+
+  
+  /* a more elegant approach not sure why we always use 
+        wierd proofs like %1 */
+
+
+  function isSquare(n) {
+    return Math.sqrt(n) % 1 === 0;
+  }
+
+
+
+  //also this which is such a easy function to visualize
+  //very readable to me
+
+  const isSquare = n => Number.isInteger(Math.sqrt(n));
