0bff0981630a25ff4342bd4688f10408377e1bfd
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-04-30 01:33:29 -0700

    Add files via upload

--- reverseString.js
+++ reverseString.js
@@ -1,6 +1,15 @@
+
+//using built in methods and a very novice yet visualy clear structure. verbouse though. 
+
 function reverseIt(str) {
     let conversionToArray = str.split('');
     let reverse = conversionToArray.reverse();
     let final = reverse.join('');
     return final
   }
+
+//simply chaining these methods all together. 
+  
+function reverseIt(str) {
+    return str.split('').reverse().join('');
+}
