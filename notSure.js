1d856b3bf14e94f7b28a6ab1241b3b488c12df56
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-05-07 23:52:22 -0700

    Add files via upload

---
+++ notSure.js
@@ -0,0 +1,9 @@
+
+
+//gotta love those stack overflow solutions. 
+
+const numToSeparate = 12345;
+
+const arrayOfDigits = Array.from(String(numToSeparate), Number);
+
+console.log(arrayOfDigits);   //[1,2,3,4,5]
