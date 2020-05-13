87372cc94b7e63abc06e47f86ce2749266cd5d77
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-05-13 11:48:46 -0700

    Add files via upload

--- squareEachDigit.js
+++ squareEachDigit.js
@@ -1,5 +1,5 @@
 let numberAss='456'                 //the number that will be passed in by a function call later
-let newArray=[]                         //this is how we isolate each integer
+let newArray=[]                         //this is how we isolate each digit in the integer
 for ( let i=0; i<numberAss.length; i++){    // iterate through the passed in integer
    
     newArray.push(numberAss[i]*numberAss[i]);    //pushing the result of each digit multiplied by itself to the new Array. 
