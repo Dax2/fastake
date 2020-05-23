4d3a651b00b6be3ee39629f7011dba5d8abddc5d
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-05-22 17:04:07 -0700

    Create arrayManipulation.js

---
+++ arrayManipulation.js
@@ -0,0 +1,7 @@
+let yaya=[]
+yaya.push(4,5,6,7,8,9);
+yaya.push(0,1,2,3);
+consol.log(...yaya)
+yaya.pop() //removes highest index of array
+yaya.pop(0) //removes the information stored in array index position 0 | in this case the digit 4
+yaya.shift() // removes from the lowest array index position. 
