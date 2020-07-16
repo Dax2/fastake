34721566ea03a8ea184ad7c4ed310bb8d234f978
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-07-16 16:17:18 -0700

    Add files via upload

---
+++ scantron.js
@@ -0,0 +1,17 @@
+function checkExam(array1, array2) {
+    var score = 0;
+      for (var i = 0; i < array2.length; i++){
+          if (array2[i] === ""){
+             score += 0
+          } else if (array1[i] === array2[i]){
+              score += 4
+          } else if (array1[i] !== array2[i]){
+             score -= 1
+          }
+      } 
+        if (score >= 0){
+          return score;
+        } else {
+          return 0
+        }
+   }
