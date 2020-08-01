c34c053b2ac420f70432335c945bb102442d2dfc
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-07-31 18:10:20 -0700

    Create randomArrow.js

---
+++ randomArrow.js
@@ -0,0 +1,26 @@
+Math.random
+ƒ random() { [native code] }
+let randomNumber=()=> Math.random
+undefined
+randomNumber()
+ƒ random() { [native code] }
+let randomNumber=()=> Math.random()
+undefined
+randomNumber()
+0.525348134902186
+let randomNumber= a => a *  Math.random()
+undefined
+randomNumber(3)
+1.4192190607934052
+let randomArrow=()=> Math.random()
+undefined
+randomArrow()
+0.995301970987698
+let randomArrow=(a,b)=> Math.floor(Math.random() * a * b)
+undefined
+randomArrow(2,5)
+6
+let randomArrow=(a,b)=> Math.floor(a * b * Math.random())
+undefined
+randomArrow(2,5)
+5
