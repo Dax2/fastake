f8f2c29b4fe9dec724960a57608bd5d700bb6ce5
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-08-04 08:23:55 -0700

    Create arrowFun.js

---
+++ arrowFun.js
@@ -0,0 +1,55 @@
+let randomNumber=()=> Math.random
+undefined
+randomNumber()
+ƒ random() { [native code] }
+let randomNumber=()=> Math.random()
+undefined
+randomNumber()
+0.525348134902186
+let randomNumber= a => a *  Math.random()
+undefined
+randomNumber(3)
+1.4192190607934052
+let randomArrow=()=> Math.random()
+undefined
+randomArrow()
+0.995301970987698
+let randomArrow=(a,b)=> Math.floor(Math.random() * a * b)
+undefined
+randomArrow(2,5)
+6
+let randomArrow=(a,b)=> Math.floor(a * b * Math.random())
+undefined
+randomArrow(2,5)
+5
+let notNeg= num=> num>=0;
+undefined
+notNeg(3)
+true
+let notNeg= num=> {
+    if (num>=0){ return "yay hoe"}
+    
+}
+undefined
+)
+VM2393:1 Uncaught SyntaxError: Unexpected token ')'
+let notNeg= num=> {
+    if (num>=0){ return "yay hoe"}
+    else ;
+}
+undefined
+let notNeg= num=> {
+    if (num>=0){ return "yay hoe"}
+    else return;
+}
+undefined
+let notNeg= num=> {
+    if (num>=0){ return "yay hoe"}
+    else num>=0;
+}
+undefined
+let notNeg= num=> {
+    if (num>=0){ return "yay hoe"}
+    else return false; 
+}
+undefined
