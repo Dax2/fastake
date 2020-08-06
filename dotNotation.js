39af199e56e380e102a618a5ccddbfd59c4026b4
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-08-06 16:31:20 -0700

    Create dotNotation.js

---
+++ dotNotation.js
@@ -0,0 +1,7 @@
+//the dot operator is odd. Initially thought you could only use it on objects and variables but you can acually directly manipulate a string without first making it a variable 
+
+let greeting= "hello";      //this how I would typically do this.
+
+greeting.length             // returns 5
+
+"hello".length              //however this gets 5 also. just SEEMS sloppy to me?
