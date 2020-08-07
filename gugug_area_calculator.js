ed35e297fc2b1c5ef337f8e00e6db24a096284b5
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-08-06 20:53:49 -0700

    Update gugug_area_calculator.js

--- gugug_area_calculator.js
+++ gugug_area_calculator.js
@@ -1,4 +1,8 @@
-let gugug=(a,b)=> {
+// first example is not refactored. started with simple a+b function to practice some logic and arrows etc. 
+
+let theHeightMustNotBeZero= new Error('woops bitcho, you fucked up. the height must be more than zero')  //changing it to an area calculator and adding some custom error codes. 
+
+let gugug=(a,b)=> {         //
     if (a<0||b<0){
         return theHeightMustNotBeZero;
     } else 
