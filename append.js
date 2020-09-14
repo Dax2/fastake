a94f02993723144d4416826b60d6989933dd5490
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-09-14 03:28:14 -0700

    Create append.js

---
+++ append.js
@@ -0,0 +1,25 @@
+Failed to load resource: net::ERR_FAILED
+let name = jon
+for ( let i=0; i<10; i++){
+    name +=1;
+    console.log(name);
+}
+VM227:1 Uncaught ReferenceError: jon is not defined
+    at <anonymous>:1:12
+(anonymous) @ VM227:1
+let name = "jon"
+for ( let i=0; i<10; i++){
+    name +=1;
+    console.log(name);
+}
+VM232:4 jon1
+VM232:4 jon11
+VM232:4 jon111
+VM232:4 jon1111
+VM232:4 jon11111
+VM232:4 jon111111
+VM232:4 jon1111111
+VM232:4 jon11111111
+VM232:4 jon111111111
+VM232:4 jon1111111111
+undefined
