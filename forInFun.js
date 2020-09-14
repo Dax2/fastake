52f300594c33179c85333a3bd404ae81668eb6d5
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-09-13 22:27:35 -0700

    Create forInFun.js

---
+++ forInFun.js
@@ -0,0 +1,42 @@
+let allUsers = {
+    user:"jon",
+
+}
+undefined
+let allUsers = {
+    user0:"jon",
+    user1:"dan",
+
+}
+undefined
+let allUsers = {
+    user0:"jon",
+    user1:"dan",
+}
+undefined
+function getAllUsers(list){
+    for (let property in list){
+        console.log(`${property}: ${list[property]}`);
+    }
+}
+undefined
+getAllUsers(alllUsers)
+VM517:1 Uncaught ReferenceError: alllUsers is not defined
+    at <anonymous>:1:13
+(anonymous) @ VM517:1
+getAllUsers(allUsers)
+VM479:3 user0: jon
+VM479:3 user1: dan
+undefined
+getAllUsers()
+undefined
+let joe= {
+    name:"dopa",
+    age:33,
+    fuck:"never",
+}
+undefined
+getAllUsers(joe)
+VM479:3 name: dopa
+VM479:3 age: 33
+VM479:3 fuck: never
