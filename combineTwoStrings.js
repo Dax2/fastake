675039bcaa23364ea40b0a100128f5daa0f7c68b
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-11-05 02:49:58 -0800

    Update combineTwoStrings.js

--- combineTwoStrings.js
+++ combineTwoStrings.js
@@ -2,5 +2,12 @@
 //here your passing in "anything" and "foo"
 //the result will be "anythingfoo"
 
+//let combineTwoStrings=(str1,str2)=> str1+str2; console.log("combination successful"). adding clog was behaving oddly, untill formated as follows...
 
-let combineTwoStrings=(str1,str2)=> str1+str2;
+let combineTwoStrings=(str1,str2)=> {
+                console.log("combination successful");             //problem here is this clog doesnt indicate a real validation. no real validation logic. just a message.  
+               return str1+str2;
+                                    }
+
+
+// how long does it take for blue jello to chill? (I mean solidify). 
