5cab9aec68de720ce47a97a0eeabd385c07e0758
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-11-05 02:39:41 -0800

    Create combineTwoStrings.js

---
+++ combineTwoStrings.js
@@ -0,0 +1,6 @@
+// this is an arrow function that combines two strings.
+//here your passing in "anything" and "foo"
+//the result will be "anythingfoo"
+
+
+let combineTwoStrings=(str1,str2)=> str1+str2;
