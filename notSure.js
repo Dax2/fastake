a1b060a1955e4e98dd2863acfaa8501669502dfb
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-11-13 03:40:57 -0800

    Update notSure.js

--- notSure.js
+++ notSure.js
@@ -4,6 +4,6 @@
 
 const numToSeparate = 12345;
 
-const arrayOfDigits = Array.from(String(numToSeparate), Number);
+const arrayOfDigits = Array.from(String(numToSeparate), Number);      //ok so what Im learning here is that you need to nest things to make a sort of order of operations. reminds me of the {{((({{((()))}})))}} problem. 
 
 console.log(arrayOfDigits);   //[1,2,3,4,5]
