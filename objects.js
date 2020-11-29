d1e039726f9092d451be4143c6a80dd6ff16975f
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-11-29 01:11:20 -0800

    Update objects.js

--- objects.js
+++ objects.js
@@ -4,3 +4,21 @@ let war = {                     //I am trying to make an object inside an object
     status: {"peace treaty", "active war", "negotiations"},
     
 }
+
+
+// success. simply changed the {} to [].
+
+let war = {
+    name: "dan",
+    weapons: {"battle axe","mace", "deathpunch"},
+    status: {"peace treaty", "active war", "negotiations"},
+    
+}
+VM293:3 Uncaught SyntaxError: Unexpected token ','
+let war = {
+    name: "dan",
+    weapons: ["battle axe","mace", "deathpunch"],                       // as desired, war.weapons[0]-->"battle axe" and so on. using the index of the array to select.
+    status: ["peace treaty", "active war", "negotiations"],
+    
+}
+undefined
