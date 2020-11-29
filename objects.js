5bca4501b9288d9f0401b1dc6c7b78e14ded2fbe
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-11-29 01:06:44 -0800

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,6 @@
+let war = {                     //I am trying to make an object inside an object? an array of arrays but I want dot notation to access war.weapons[0]-->"battle axe"
+    name: "dan",
+    weapons: {"battle axe","mace", "deathpunch"},
+    status: {"peace treaty", "active war", "negotiations"},
+    
+}
