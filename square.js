7ddd0bb966b9415ef6ba94575139329a514c26e1
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-12-13 17:26:25 -0800

    Update square.js

--- square.js
+++ square.js
@@ -1,6 +1,6 @@
-var isSquare = function(n){
-    half = n/2;                 //get half the number
-    if (n == half * 2 && n >= 0){        // conditional statement that totally misses the mark
+var isSquare = function(n){             //setting a var equal to a function. es6 we use let instead of var. 
+    half = n/2;                         //get half the number
+    if (n == half * 2 && n >= 0){       // conditional statement that totally misses the mark
       return true
   }
     else
