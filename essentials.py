2d210727d26b73ce170ca23b6443a0295ce98e4d
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-12-19 03:26:29 -0800

    Update essentials.py

--- essentials.py
+++ essentials.py
@@ -1,18 +1,18 @@
 #this is how you comment in py
 # so as in any real language we can't just run our code in the console in google browser. 
 
-we have to write the code in a file. save. then run that file. 
+#we have to write the code in a file. save. then run that file. 
 
-or
+#or
 
-you can activate python in the terminal and run it there for some short runs.
-
-
-spacing MUST BE CONSISTENT. In school we used tab. most I THINK use at least four spaces but python doesnt care as long as you keep it the same so even if you used one space it would run. 
+#you can activate python in the terminal and run it there for some short runs.
 
 
+#spacing MUST BE CONSISTENT. In school we used tab. most I THINK use at least four spaces but python doesnt care as long as you keep it the same so even if you used one space it would run. 
 
 
 
-for x in "jarjarbinks":   // this looks just like the for in added to js recently. Why are there so many similarities 
+
+
+for x in "jarjarbinks":   # this looks just like the for in added to js recently. Why are there so many similarities 
   print(x) 
