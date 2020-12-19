14bedaf8ca15e85fd3dcb34439f2a58a6af3d33e
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-12-19 14:32:13 -0800

    Update essentials.py

--- essentials.py
+++ essentials.py
@@ -16,3 +16,32 @@
 
 for x in "jarjarbinks":   # this looks just like the for in added to js recently. Why are there so many similarities 
   print(x) 
+
+  
+  
+  > print("hello world")
+hello world
+>>> if 5>2:
+...     print("greater than two")
+... 3+3
+  File "<stdin>", line 3
+    3+3
+    ^
+SyntaxError: invalid syntax
+>>> 
+
+# below is the full output from a terminal sessionl. 
+
+danielcontreras@Daniels-MBP ~ % python3                       # typing python3 activates one of the multiple versions of python installed on my computer. python and python2(i think) also exist.
+Python 3.7.5 (default, Nov  1 2019, 02:16:23) 
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 4+4                                                                   #playing with some of what I remember. Python3 isnt very strict so you can do zainy stuff. 
+8
+>>> j=4+4
+>>> j
+8
+>>> j+3
+11
+>>> exit()                                                      #for some reason I never forget that to end an interactive session in terminal you type exit()
+danielcontreras@Daniels-MBP ~ % 
