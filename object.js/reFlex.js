43a7626377813141c5f0046edda2613d93237669
Daniel C <41881275+Dax2@users.noreply.github.com>
2021-01-07 01:21:21 -0800

    Update reFlex.js

--- object.js/reFlex.js
+++ object.js/reFlex.js
@@ -1,4 +1,61 @@
+   
+//so here I am flailing with the infamous following keyword: this. 
+
 let core=(parameter1, parameter2)=> {
       name:parameter1,
       userid:parameter2,
 }
+VM35:4 Uncaught SyntaxError: Unexpected token ':'
+let core={
+        name:"name",
+        userid:500,
+}
+undefined
+core.userid
+500
+let core={
+        name:"name",
+        userid:500,
+        function: function core(name){ name + "hello"}
+}
+undefined
+core.function("dan")
+undefined
+core.function()
+undefined
+let core={
+        name:"name",
+        userid:500,
+        function: function core(){ return this.name + "hello"};
+}
+VM366:4 Uncaught SyntaxError: Unexpected token ';'
+let core={
+        name:"name",
+        userid:500,
+        function: function core(){ return this.name + "hello"} //so here things have gone off the rails. 
+}
+undefined                                                               // no issue here but...
+core.function("dan")
+"namehello"                                                                   //so i was hoping to return "danhello" but got this.name which is still "name". function did not work. 
+let core={
+        name:"name",
+        userid:500,
+        changeName: function changeName(newName){ name=newName; 
+        return this.name + "hello"}
+}
+undefined
+core.changeName("dan")
+"namehello"
+core.changeName(dan)
+VM697:1 Uncaught ReferenceError: dan is not defined
+    at <anonymous>:1:17
+(anonymous) @ VM697:1
+let core={
+        name:"name",
+        userid:500,
+        changeName: function change(newName){ name=newName; 
+        return this.name + "hello"}
+}
+undefined
+core.changeName("dan")
+"namehello"
