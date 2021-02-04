af46c598b23238a146d92b412ef8629294d778df
Daniel C <41881275+Dax2@users.noreply.github.com>
2021-02-04 00:05:20 -0800

    Update objects.js

--- objects.js
+++ objects.js
@@ -42,4 +42,57 @@ name
 
 
 // now recently I got I think a json package when a site failed to load. 
-// looked like a bunch of js objects. so uncovered a few trail heads. led to XML and SGML(standard generalized language, new to me).  
+// looked like a bunch of js objects. so uncovered a few trail heads. led to XML and SGML(standard generalized language, new to me).
+// so I cam across this...
+
+{"widget": {                                        // I realize that this reminds me of the intro to mongodb.
+    "debug": "on",
+    "window": {                                             // just key value pairs inside of key value pairs of key value pairs...lol. 
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": { 
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}}    
+
+                                                                //The same text expressed as XML:
+
+<widget>
+    <debug>on</debug>
+    <window title="Sample Konfabulator Widget">
+        <name>main_window</name>
+        <width>500</width>
+        <height>500</height>
+    </window>
+    <image src="Images/Sun.png" name="sun1">
+        <hOffset>250</hOffset>
+        <vOffset>250</vOffset>
+        <alignment>center</alignment>
+    </image>
+    <text data="Click Here" size="36" style="bold">
+        <name>text1</name>
+        <hOffset>250</hOffset>
+        <vOffset>100</vOffset>
+        <alignment>center</alignment>
+        <onMouseUp>
+            sun1.opacity = (sun1.opacity / 100) * 90;
+        </onMouseUp>
+    </text>
+</widget>
